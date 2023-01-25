9fb2fca166e01e2b217034da5a6be61bfc2c571d
¨Leonardo <leonardoantezana59@gmail.com>
2023-01-25 11:36:09 -0300

    trayendo detalles del heroe pedido

--- src/components/CharacterDetail/CharacterDetail.jsx
+++ src/components/CharacterDetail/CharacterDetail.jsx
@@ -1,10 +1,21 @@
 import styles from './styles/CharacterDetail.module.scss';
 
 const CharacterDetail = ({ character }) => {
-  const {id, name, description, thumbnail} = character
+  const {name, description, thumbnail, comics} = character
 	return (
 		<div className={styles.characterdetail}>
-      CharacterDetail
+      <div className={styles.container__image}>
+        <img src={`${thumbnail.path}.${thumbnail.extension}`} alt={`image-${name}`}/>
+      </div>
+      <div className={styles.infoHero}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.description}>{description ? description : 'No description for now'}</p>
+        <p><span>Appearances in comics:</span> {comics.available}</p>
+        <p><span>Most recents appearances: </span></p>
+        <ul>
+          {comics.items.slice(0,3).map((comic, index) => <li key={index}>{comic.name}</li>)}
+        </ul>
+      </div>
     </div>
 	)
 };
--- src/pages/CharacterContainer/CharacterContainer.jsx
+++ src/pages/CharacterContainer/CharacterContainer.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import styles from './styles/CharacterContainer.module.scss';
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import CharacterDetail from '../../components/CharacterDetail/CharacterDetail';
 
 const CharacterContainer = () => {
   const {idCharacter} = useParams()
@@ -9,13 +10,13 @@ const CharacterContainer = () => {
   useEffect(() => {
     const getData = async () => {
       let data = await axios.get(`http://gateway.marvel.com/v1/public/characters/${idCharacter}?ts=1&apikey=4442ef4e3aae679fff0e2f6e19e96be8&hash=32bfa31ec597ede355ace93746fefec1`)
-      console.log(data)
+      setHero(data.data.data.results[0])
     }
     getData()
   }, [])
 	return (
 		<div className={styles.charactercontainer}>
-      CharacterContainer
+      {hero && <CharacterDetail character={hero}/>}
     </div>
 	)
 };
