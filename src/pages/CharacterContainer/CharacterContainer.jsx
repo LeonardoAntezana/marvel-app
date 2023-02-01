e3d500bb7c06973d2e3c2e09b70e263548a23ef5
¨Leonardo <leonardoantezana59@gmail.com>
2023-02-01 16:33:02 -0300

    solucionando request

--- src/pages/CharacterContainer/CharacterContainer.jsx
+++ src/pages/CharacterContainer/CharacterContainer.jsx
@@ -5,7 +5,7 @@ import styles from './styles/CharacterContainer.module.scss';
 
 const CharacterContainer = () => {
   const {idCharacter} = useParams()
-  const {data: hero} = useCallApi(`http://gateway.marvel.com/v1/public/characters/${idCharacter}?ts=1&apikey=4442ef4e3aae679fff0e2f6e19e96be8&hash=32bfa31ec597ede355ace93746fefec1`)
+  const {data: hero} = useCallApi(`https://gateway.marvel.com/v1/public/characters/${idCharacter}?ts=1&apikey=4442ef4e3aae679fff0e2f6e19e96be8&hash=32bfa31ec597ede355ace93746fefec1`)
 	return (
 		<div className={styles.charactercontainer}>
       {hero && <CharacterDetail character={hero[0]}/>}
